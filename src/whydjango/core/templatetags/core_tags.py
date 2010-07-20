0f39e2e78ffc8cb3900d28bcdfa6d59b34af4108
pydanny <pydanny@gmail.com>
2010-07-20 10:40:48 -0500

    Getting breadcrumbs in

--- src/whydjango/core/templatetags/core_tags.py
+++ src/whydjango/core/templatetags/core_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.flatpages.models import FlatPage
 
 from whydjango.core.models import TopLink
 
@@ -8,3 +9,13 @@ register = template.Library()
 def top_links():
     return { 'links':TopLink.objects.all() }
     
+@register.simple_tag
+def breadcrumbs(flatpage):
+    absolute_url = flatpage.get_absolute_url()
+    text = ''
+    url = ''
+    for element in absolute_url[1:-1].split('/'):
+        url += '/%s' % element
+        flatpage = FlatPage.objects.get(url='%s/' % url)
+        text += '<a href="%s/">%s</a> ' % (url, flatpage.title)
+    return text
